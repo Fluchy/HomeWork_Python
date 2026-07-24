7425ec413a0d658d89c796f0f2d990ee974a0e1a
Fluchy <fluchyx3@gmail.com>
2026-07-24 10:15:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 try:
-    a = float(input("Введите первое число\n"))
-    b = float(input("Введите второе число\n"))
-    result = a / b
-    print("Результат деления первого числа на второе:", result)
-except  ValueError:
-    print("\nОшибка нечислового значания")
+    print("\n1- Сложение")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print(num1 + num2)
+
+    print("\n2- Вычитание")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print(num1 - num2)
+
+    print("\n3- Умножение")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print(num1 * num2)
+
+    print("\n4- Деление")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print(num1 / num2)
+
+except ValueError:
+    print("Please enter a number")
+
 except ZeroDivisionError:
-    print("\nОшибка деления на 0")
-finally:
-    print("\nProgram finished")
+    print("Division by zero")
